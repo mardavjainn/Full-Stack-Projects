375d2d10b5086bce6a1394557bf8286741ed5a86
mardavjainn <mardavjain07@gmail.com>
2025-09-21 00:33:33 +0530

    Worked On Delete functionality

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import Navbar from "../Components/navbar";
 import  RateLimitedUI  from "../Components/RateLimitedUI";
-import axios from "axios";
+import NoteCard from "../Components/NoteCard";
+import api from "../lib/axios";
+import NotesNotFound from "../Components/NotesNotFound";
 import toast from "react-hot-toast";
+
 const HomePage=()=>{
-  const [isRateLimited,setIsRateLimited]=useState(true);
+  const [isRateLimited,setIsRateLimited]=useState(false);
   const [notes,setNotes]=useState([])
   const [loading,setLoading]=useState(true)
   useEffect(()=>{
     const fetchNotes=async()=>{
       try {
-        const res=await axios.get('http://localhost:5000/api/notes')
+        const res=await api.get('/notes')
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
@@ -32,14 +35,13 @@ const HomePage=()=>{
     <div className="min-h-screen">
       <Navbar/>
       {isRateLimited && <RateLimitedUI />}
-      <div className="max-w-7xl max auto p-4 mt-6">
+      <div className="mx-w-7xl max auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Notes....</div>}
+        {notes.length===0 && !isRateLimited && <NotesNotFound />}
         {notes.length>0 && !isRateLimited &&(
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note)=>(
-              <div>
-                {note.title} |{note.content}
-                </div>
+             <NoteCard key={note._id} note={note} setNotes={setNotes} /> 
             ))}
             </div>
         )}
--- frontend/src/Pages/NoteDetail.jsx
+++ frontend/src/Pages/NoteDetail.jsx
@@ -1,7 +1,14 @@
- const NoteDetail = () => {
-  return (
-    <div>NoteDetail</div>
-  )
+import {useState} from 'react';
+import { useNavigate, useParams } from 'react-router';
+const NoteDetailPage=()=>{
+  const [note,setNote]=useState(null);
+  const [loading,setLoading]=useState(true);
+  const [saving,setSaving]=useState(false);
+  const navigate =useNavigate()
+
+  const {id}=useParams()
+  console.log({id})
+  return <div>NoteDetailPage</div>
   
-}
+};
 export default NoteDetail;
