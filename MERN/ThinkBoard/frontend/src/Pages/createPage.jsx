0c5df3c3c8323461419b5467586744d71bb85db6
mardavjainn <mardavjain07@gmail.com>
2026-07-10 01:00:12 +0530

    Minor Changes

--- MERN/ThinkBoard/frontend/src/Pages/createPage.jsx
+++ MERN/ThinkBoard/frontend/src/Pages/createPage.jsx
@@ -19,7 +19,7 @@ const CreatePage=()=>{
     }
     setLoading(true);
     try {
-      await api.post("http://localhost:5000/api/notes",{
+      await api.post("/notes",{
         title,
         content
       })
